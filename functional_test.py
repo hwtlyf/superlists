bb136a58a66814672f418fac2254f9ba6a7898e0
hwtlyf <weitao0212@163.com>
2019-04-09 14:02:53 +0800

    add the unittest

--- functional_test.py
+++ functional_test.py
@@ -1,8 +1,22 @@
+#!/usr/bin/env python
+#:author: @monkeyrp
+# """ this is the test for practice"""
+
 from selenium import webdriver
+import unittest
 
-brower = webdriver.Firefox()
-brower.get("http://localhost:8000")
+class NewVistorTest(unittest.TestCase):
+	"""docstring for NewVistorTest"""
+	def setUp(self):
+		self.brower = webdriver.Firefox()
+	
+	def tearDown(self):
+		self.brower.quit()
 
-assert 'Django' in brower.title
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.brower.get("http://localhost:8000")
+		self.assertIn('To_Do', self.brower.title)
+		self.fail("finish the test!")
 
-#test for git
+if __name__ == '__main__':
+	unittest.main(warnings="ignore")
